feb0fdc770e8fd1d44a99cca052bd3122aa73505
Michael Berman <michael.v.berman@gmail.com>
2018-03-08 13:27:04 -0500

    add index and post actions to user controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,15 +3,26 @@ const bodyParser = require('bodyParser');
 
 // index
 exports.getUsers = (req, res) => {
-
+  User.find({}, (err, users) => {
+    if (!err) {
+      res.end(users);
+    } else {
+      console.log(err);
+    }
+  });
 };
 
 // Show
 exports.getUser = (req, res) => {
-
+  // TODO: figure out how to find a user depending on request body
 };
 
 // Post
 exports.createUser = (req, res) => {
-
+  const newUser = new User ({
+    // TODO: input user parameters
+  });
+  newUser.save().then(() => {
+    res.end();
+  });
 };
