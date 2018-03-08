7972d3338840656b9d17680db58233562e0ffef6
Michael Berman <michael.v.berman@gmail.com>
2018-03-08 12:41:33 -0500

    add file for env vars

---
+++ server/env.js
@@ -0,0 +1,4 @@
+require('dotenv').config({ path: './variables.env' });
+
+export const port = process.env.PORT;
+export const database = process.env.PORT;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import fs from 'fs';
 import express from 'express';
-
+import { port } from './env';
 const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile('../client/index.html');
 });
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
