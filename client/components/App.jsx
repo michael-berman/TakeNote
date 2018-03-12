e6ca4ce24fd7dee0efeca1b77a736cb3bc292242
Michael Berman <michael.v.berman@gmail.com>
2018-03-12 17:50:59 -0400

    add frontend routes for session form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SessionFormContainer from './session_form/session_form_container';
+// TODO: add auth routes
 
 const App = () => {
   return (
     <div>
-      <p>hello from react</p>
+      <Switch>
+        <Route path='/login' component={SessionFormContainer} />
+      </Switch>
     </div>
   );
 };
