ee1896e1e679b50115886290c5de69c53b79960f
Michael Berman <michael.v.berman@gmail.com>
2018-03-12 12:30:56 -0400

    configure store.js

--- client/store/store.js
+++ client/store/store.js
@@ -1,1 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk, logger];
+
+const configureStore = (preloadedState = {}) => {
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middlewares)
+  );
+};
+
+export default configureStore;
--- client/take_note.jsx
+++ client/take_note.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-// import co
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
