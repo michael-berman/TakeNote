b6d7334c696a56c2b2bf3888c2f76d966b101c3d
Michael Berman <michael.v.berman@gmail.com>
2018-03-12 17:36:44 -0400

    add session component container

---
+++ client/components/session_form/session_form_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import SessionForm from './session_form';
+// TODO: import session actions
+
+const mapStatetoProps = (state) => {
+  return null;
+  // TODO: add Boolean logged in
+};
+
+const mapDispatchtoProps = (dispatch, { location }) => {
+  const locationString = location.pathname.slice(1);
+  const formType = locationString.charAt(0).toUpperCase() +
+                    locationString.slice(1);
+  return {
+    // TODO: add submit form action and clear session errors
+    formType
+  };
+};
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(SessionForm);
