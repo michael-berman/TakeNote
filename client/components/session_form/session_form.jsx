0308534631328b5734c4669d654e37938c005237
Michael Berman <michael.v.berman@gmail.com>
2018-03-12 17:31:47 -0400

    add session form component

---
+++ client/components/session_form/session_form.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link, withRouter, Redirect} from 'react-router-dom';
+
+class SessionForm extends React.component {
+  constructor(props) {
+    super(props);
+    if (this.props.formType === 'login') {
+      this.state = {
+        username: "",
+        password: ""
+      };
+    } else {
+      this.state = {
+        email: "",
+        username: "",
+        password: ""
+      };
+    }
+  }
+
+  update(field){
+    return e => {
+      return this.setState({ [field]: e.target.value });
+    };
+  }
+
+  render() {
+    const formButton = (this.props.formType === "Signup")?
+      "Sign Up" : "Login";
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="Email"/>
+          <input type="password" placeholder="password" />
+          <button>{formButton}</button>
+        </form>
+      </div>
+    );
+  }
+}
+// TODO: add classnames to form and input
+// TODO: add demo login
+
+
+export default withRouter(SessionForm);
