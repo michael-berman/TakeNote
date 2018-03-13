7a2a32926fd6a1fe1a0e38406a5507b91756826e
Michael Berman <michael.v.berman@gmail.com>
2018-03-13 11:59:12 -0400

    add session action to session reducer

--- client/reducers/session_reducer.js
+++ client/reducers/session_reducer.js
@@ -1,7 +1,5 @@
 import merge from 'lodash/merge';
-
-// import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
-// TODO: add session actions
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const _nullUser = Object.freeze({
   currentUser: null
@@ -10,9 +8,8 @@ const _nullUser = Object.freeze({
 const sessionReducer = (state=_nullUser, action) => {
   Object.freeze(state);
   switch (action.type) {
-    // case RECEIVE_CURRENT_USER:
-    //   return merge({}, {currentUser: action.currentUser});
-    // TODO: add recieve current user action
+    case RECEIVE_CURRENT_USER:
+      return merge({}, {currentUser: action.currentUser});
     default:
       return state;
   }
