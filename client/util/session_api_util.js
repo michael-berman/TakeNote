4a6dd25f322662424223f2757dbbf8bc35374676
Michael Berman <michael.v.berman@gmail.com>
2018-03-13 11:47:18 -0400

    add api routes to session on backend

--- client/util/session_api_util.js
+++ client/util/session_api_util.js
@@ -1,2 +1,31 @@
 import $ from 'jquery';
 // TODO: Create ajax requests for signup, login, logout
+
+export const login = user => {
+  return (
+    $.ajax({
+      method: 'post',
+      url: 'api/session',
+      data: { user }
+    })
+  );
+};
+
+export const signup = user => {
+  return (
+    $.ajax({
+      method: 'post',
+      url: 'api/users',
+      data: { user }
+    })
+  );
+};
+
+export const logout = () => {
+  return (
+    $.ajax({
+      method: 'delete',
+      url: 'api/session'
+    })
+  );
+};
--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -3,9 +3,9 @@ import sessionController from './controllers/sessionController';
 
 const router = express.router;
 
-router.post('/login', sessionController.loginUser);
+router.post('api/login', sessionController.loginUser);
 
-router.post('/signup', sessionController.signupUser);
+router.post('api/signup', sessionController.signupUser);
 
 // TODO: add in method override for delete (logout)
 
